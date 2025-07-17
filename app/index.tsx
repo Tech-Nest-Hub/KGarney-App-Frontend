95877cffd85e140d8cc58e0b66a5e8ad4512e5d2
Ghost9841 <asubedi778@gmail.com>
2025-07-17 08:52:29 +0545

    Added Native Tailwind BUt not working

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,24 @@
+import '@/app/global.css';
 import DarkMode from "@/components/DarkMode";
-import { SafeAreaView, Text, View } from "react-native";
+import FlatCards from "@/components/FlatCards";
+import { SafeAreaView, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
   return (
    <SafeAreaView>
+    <ScrollView>
+
     <View>
 
-    <Text>Hello Aakash!</Text>
+    <Text style={{fontSize: 20, fontWeight: 'bold', justifyContent: 'center', alignItems: 'center', flex: 1, alignSelf: 'center', flexDirection: 'row'}}>Hello KTA Haru!</Text>
     <DarkMode/>
+    <FlatCards/>
     </View>
+    <View className='flex-1 justify-center items-center'>
+      <Text className='text-2xl font-bold'>Welcome to KTA Haru</Text>
+      <Text className='text-lg'>This is a sample app using Expo and Nativewind</Text>
+    </View>
+    </ScrollView>
    </SafeAreaView>
   );
 }
---
+++ components/FlatCards.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const FlatCards = () => {
+  return (
+    <View>
+      <Text>FlatCards</Text>
+    </View>
+  )
+}
+
+export default FlatCards
