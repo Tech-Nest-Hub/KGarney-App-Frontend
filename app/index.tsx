da217c50352c869802c56f45e48ba4577efd7bc7
Ghost9841 <asubedi778@gmail.com>
2025-07-19 18:09:19 +0545

    Starting

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,11 @@
 import '@/app/global.css';
-import DarkMode from "@/components/DarkMode";
-import FlatCards from "@/components/FlatCards";
-import { SafeAreaView, ScrollView, Text, View } from "react-native";
+import AppBar from '@/components/App';
+import { SafeAreaView } from "react-native";
 
 export default function Index() {
   return (
    <SafeAreaView>
-    <ScrollView>
-
-    <View>
-
-    <Text style={{fontSize: 20, fontWeight: 'bold', justifyContent: 'center', alignItems: 'center', flex: 1, alignSelf: 'center', flexDirection: 'row'}}>Hello KTA Haru!</Text>
-    <DarkMode/>
-    <FlatCards/>
-    </View>
-    <View className='flex-1 justify-center items-center'>
-      <Text className='text-2xl font-bold'>Welcome to KTA Haru</Text>
-      <Text className='text-lg font-extrabold'>This is a sample app using Expo and Nativewind</Text>
-    </View>
-    </ScrollView>
+    <AppBar />
    </SafeAreaView>
   );
 }
